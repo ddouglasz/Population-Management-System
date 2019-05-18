10cc66122fcae374c8a792172d591974d98810af
Douglas Egiemeh <douglasegiemeh@gmail.com>
2019-05-18 11:34:13 +0100

    - Implement setup server - Design schema

---
+++ models/populationModel.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from 'mongoose';
+
+const populationSchema = new Schema({
+    location: { type: String, required: true, trim: true },
+    malePopulation: { type: Number, required: true },
+    femalePopulation: { type: Number, required: true },
+    totalPopulation: { type: Number, required: true },
+    subLocations: []
+},
+    {
+        timeStamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+    });
+
+const Population = mongoose.model('population', populationSchema);
+
+export default Population;
